d65847b2aec82dbb49f411c2fc4707c6fbda8eef
jacky6016 <chlin6016@gmail.com>
2021-05-20 10:41:44 -0700

    Use Passport JS and Google Strategy to handle OAuth

--- server/index.js
+++ server/index.js
@@ -1,25 +1,41 @@
 // access common JS modules
 const express = require('express');
-// ES 2015 syntax
-// import express from 'express'
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys') // OAuth keys
 
 // Create an express app to handle incoming requests
 const app = express();
 
-// an arrow function (similar to lambda function in Python)
-// get(): register for http GET requrest (other: post(), put(), delete(), patch())
-// the second argument of get() is the route handler
-// '/': accessed route
-// req: JS object representing the incoming request
-// res: JS object representing the outgoing response
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+// Register the instance of Google Passport strategy to passport JS
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    // callback function
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// Route handler to access OAuth
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email'] // ask google for us to access the user's profile and email
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Prod: Run the app on a certain port configured by Heroku (a runtime environment variable)
 // Dev: Run on PORT 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
-// app.listen(5000); // run on port 5000
+
 
 
